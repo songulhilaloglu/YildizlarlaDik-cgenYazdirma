6574726934552840350991c954a5894f4b1560d4
songulhilaloglu <103853967+songulhilaloglu@users.noreply.github.com>
2022-09-07 00:40:18 +0300

    Add project files.

---
+++ YildizlarlaDikÜcgenYazdirma/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace YildizlarlaDikÜcgenYazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dik ücgen yazdirma
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();    
+            }
+
+            // Ters dik ücgen yazdirma
+            for (int i = 0; i < 5; i++)
+            {
+                for (int k = 5; k >= i; k--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
